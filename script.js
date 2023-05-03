e7985b453ea43d75932532d769cc5dffed45e217
Mathis-Mathis <mathisu.mousli@gmail.com>
2023-05-03 15:22:58 +0200

    .

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//annimation page accueil
+
 const footer = document.querySelector('footer');
 const footerHeight = footer.getBoundingClientRect().height;
 
@@ -13,3 +15,40 @@ window.addEventListener('scroll', () => {
     hill5.style.left = value * 1.5 + 'px';
   }
 });
+
+
+//défilement vers les pages via la navbar
+
+// Sélectionnez l'élément "a" correspondant au lien "about"
+const link = document.querySelector('a[href="#about"]');
+
+// Ajoutez un événement "click" à l'élément "a"
+link.addEventListener('click', (event) => {
+  event.preventDefault(); // Empêche le comportement par défaut du lien
+  
+  // Récupère la position de la section "about" par rapport au haut de la page
+  const aboutSection = document.querySelector('#about');
+  const position = aboutSection.offsetTop;
+
+  // Faites défiler la page vers la position de la section "about"
+  window.scrollTo({
+    top: position,
+    behavior: 'smooth'
+  });
+});
+
+
+
+//boutton pour remonter la page
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
